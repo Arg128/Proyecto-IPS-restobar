e07a3f902c38a362deb3d43ced91d6b5ab9f00fb
Arg128 <aretamozoc@unsa.edu.pe>
2026-06-25 12:19:50 -0500

    Subiendo admin-frontend apuntando a REACT-API-URL

--- apps/admin-frontend/src/index.js
+++ apps/admin-frontend/src/index.js
@@ -7,7 +7,7 @@ import * as serviceWorker from './serviceWorker';
 import store from './store';
 import axios from "axios";
 
-axios.defaults.baseURL = process.env.REACT_APP_API_URL || `${process.env.HOST || "http://localhost"}:${process.env.ADMIN_BACKEND_PORT || 5000}`;
+axios.defaults.baseURL = process.env.REACT_APP_API_URL || `${process.env.HOST || "http://localhost"}:${process.env.RESTO_BACKEND_PORT || 5000}`;
 
 ReactDOM.render(
 
@@ -17,7 +17,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
