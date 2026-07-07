9839fadcde858481b92123ea01b9f29a7844b68d
Arg128 <aretamozoc@unsa.edu.pe>
2026-07-06 19:58:20 -0500

    Added JWT Validator

--- apps/resto-backend/middleware/authMiddleware.js
+++ apps/resto-backend/middleware/authMiddleware.js
@@ -8,10 +8,8 @@ exports.protect = async (req, res, next) => {
     if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
         try {
             token = req.headers.authorization.split(" ")[1];
-            // const decoded = jwt.verify(token, process.env.JWT_SECRET);
-            //const user = db.select().from(users).where(eq(users.id, decoded.id)).get();
-
-            const user = db.select().from(users).where(eq(users.id)).get();
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            const user = db.select().from(users).where(eq(users.id, decoded.id)).get();
             if (!user) {
                 res.status(401);
                 throw new Error("Not authorized");
