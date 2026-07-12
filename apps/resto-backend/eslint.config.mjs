6ccc64ec2ba67b112a5a700f3a69ff2daefd6711
Arg128 <aretamozoc@unsa.edu.pe>
2026-07-11 22:47:10 -0500

    Added 'eslint-plugin-jest' to workflow

--- apps/resto-backend/eslint.config.mjs
+++ apps/resto-backend/eslint.config.mjs
@@ -4,7 +4,15 @@ import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js, jest },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: globals.browser,
+      globlas: globals.jest,
+    } },
+  { files: ["**/*.js"], languageOptions: {
+      globals: globals.jest,
+  } },
   tseslint.configs.recommended,
 ]);
